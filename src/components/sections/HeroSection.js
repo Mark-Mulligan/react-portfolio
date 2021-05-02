6525a7f96fd857b898ca3ebceae3ab6e6566d809
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-02 15:00:39 -0500

    made hero section responsive

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,93 +1,153 @@
-import React from "react";
+import React, { useEffect } from "react";
+import anime from "animejs";
 import "./HeroSection.css";
 
 const HeroSection = () => {
+  const animatedImage = () => {
+    let timeline = anime.timeline({ loop: true });
+
+    timeline
+      .add({
+        targets: ".square",
+        opacity: [
+          { value: 0.5, easing: "easeInSine", duration: 500 },
+          { value: 1, easing: "easeInOutQuad", duration: 1000 },
+        ],
+        scale: [
+          { value: 0.92, easing: "easeInSine", duration: 500 },
+          { value: 1, easing: "easeInOutQuad", duration: 1000 },
+        ],
+        delay: anime.stagger(330, { grid: [9, 7], from: "first" }),
+      })
+      .add({
+        targets: ".square",
+        opacity: [
+          { value: 0.5, easing: "easeInSine", duration: 500 },
+          { value: 1, easing: "easeInOutQuad", duration: 1000 },
+        ],
+        scale: [
+          { value: 0.92, easing: "easeInSine", duration: 500 },
+          { value: 1, easing: "easeInOutQuad", duration: 1000 },
+        ],
+        delay: anime.stagger(330, { grid: [9, 7], from: "last" }),
+      })
+      .add({
+        targets: ".square",
+        opacity: [
+          { value: 0.5, easing: "easeInSine", duration: 500 },
+          { value: 1, easing: "easeInOutQuad", duration: 1000 },
+        ],
+        scale: [
+          { value: 0.92, easing: "easeInSine", duration: 500 },
+          { value: 1, easing: "easeInOutQuad", duration: 1000 },
+        ],
+        delay: anime.stagger(330, { grid: [9, 7], from: "center" }),
+      });
+  };
+
+  useEffect(() => {
+    /* animatedImage(); */
+  }, []);
+
   return (
-    <div class="hero-section">
-      <div class="animated-img-container">
-        <div class="animated-img">
-          <div class="my-row row-0">
-            <div class="column-0 square"></div>
-            <div class="column-1 square"></div>
-            <div class="column-2 square"></div>
-            <div class="column-3 square"></div>
-            <div class="column-4 square"></div>
-            <div class="column-5 square"></div>
-            <div class="column-6 square"></div>
-            <div class="column-7 square"></div>
-            <div class="column-8 square"></div>
+    <div className="hero-section">
+      <div className="animated-img-container">
+
+        <div className="center-square-wrapper">
+          <div className="center-square">
+            <div className="hero-text title">
+              <h1>Mark Mulligan</h1>
+            </div>
+
+            <div className="hero-text developer">
+              <h1>Web Developer</h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="animated-img">
+          <div className="my-row row-0">
+            <div className="column-0 square"></div>
+            <div className="column-1 square"></div>
+            <div className="column-2 square"></div>
+            <div className="column-3 square"></div>
+            <div className="column-4 square"></div>
+            <div className="column-5 square"></div>
+            <div className="column-6 square"></div>
+            <div className="column-7 square"></div>
+            <div className="column-8 square"></div>
           </div>
 
-          <div class="my-row row-1">
-            <div class="column-0 square"></div>
-            <div class="column-1 square"></div>
-            <div class="column-2 square"></div>
-            <div class="column-3 square"></div>
-            <div class="column-4 square"></div>
-            <div class="column-5 square"></div>
-            <div class="column-6 square"></div>
-            <div class="column-7 square"></div>
-            <div class="column-8 square"></div>
+          <div className="my-row row-1">
+            <div className="column-0 square"></div>
+            <div className="column-1 square"></div>
+            <div className="column-2 square"></div>
+            <div className="column-3 square"></div>
+            <div className="column-4 square"></div>
+            <div className="column-5 square"></div>
+            <div className="column-6 square"></div>
+            <div className="column-7 square"></div>
+            <div className="column-8 square"></div>
           </div>
 
-          <div class="my-row row-2">
-            <div class="column-0 square"></div>
-            <div class="column-1 square"></div>
-            <div class="column-2 square"></div>
-            <div class="column-3 square"></div>
-            <div class="column-4 square"></div>
-            <div class="column-5 square"></div>
-            <div class="column-6 square"></div>
-            <div class="column-7 square"></div>
-            <div class="column-8 square"></div>
+          <div className="my-row row-2">
+            <div className="column-0 square"></div>
+            <div className="column-1 square"></div>
+            <div className="column-2 square"></div>
+            <div className="column-3 square"></div>
+            <div className="column-4 square"></div>
+            <div className="column-5 square"></div>
+            <div className="column-6 square"></div>
+            <div className="column-7 square"></div>
+            <div className="column-8 square"></div>
           </div>
 
-          <div class="my-row row-3">
-            <div class="column-0 square"></div>
-            <div class="column-1 square"></div>
-            <div class="column-2 square"></div>
-            <div class="column-3 square"></div>
-            <div class="column-4 square"></div>
-            <div class="column-5 square"></div>
-            <div class="column-6 square"></div>
-            <div class="column-7 square"></div>
-            <div class="column-8 square"></div>
+          <div className="my-row row-3">
+            <div className="column-0 square"></div>
+            <div className="column-1 square"></div>
+            <div className="column-2 square"></div>
+            <div className="column-3 square"></div>
+            <div className="column-4 square"></div>
+            <div className="column-5 square"></div>
+            <div className="column-6 square"></div>
+            <div className="column-7 square"></div>
+            <div className="column-8 square"></div>
           </div>
 
-          <div class="my-row row-4">
-            <div class="column-0 square"></div>
-            <div class="column-1 square"></div>
-            <div class="column-2 square"></div>
-            <div class="column-3 square"></div>
-            <div class="column-4 square"></div>
-            <div class="column-5 square"></div>
-            <div class="column-6 square"></div>
-            <div class="column-7 square"></div>
-            <div class="column-8 square"></div>
+          <div className="my-row row-4">
+            <div className="column-0 square"></div>
+            <div className="column-1 square"></div>
+            <div className="column-2 square"></div>
+            <div className="column-3 square"></div>
+            <div className="column-4 square"></div>
+            <div className="column-5 square"></div>
+            <div className="column-6 square"></div>
+            <div className="column-7 square"></div>
+            <div className="column-8 square"></div>
           </div>
 
-          <div class="my-row row-5">
-            <div class="column-0 square"></div>
-            <div class="column-1 square"></div>
-            <div class="column-2 square"></div>
-            <div class="column-3 square"></div>
-            <div class="column-4 square"></div>
-            <div class="column-5 square"></div>
-            <div class="column-6 square"></div>
-            <div class="column-7 square"></div>
-            <div class="column-8 square"></div>
+          <div className="my-row row-5">
+            <div className="column-0 square"></div>
+            <div className="column-1 square"></div>
+            <div className="column-2 square"></div>
+            <div className="column-3 square"></div>
+            <div className="column-4 square"></div>
+            <div className="column-5 square"></div>
+            <div className="column-6 square"></div>
+            <div className="column-7 square"></div>
+            <div className="column-8 square"></div>
           </div>
 
-          <div class="my-row row-6">
-            <div class="column-0 square"></div>
-            <div class="column-1 square"></div>
-            <div class="column-2 square"></div>
-            <div class="column-3 square"></div>
-            <div class="column-4 square"></div>
-            <div class="column-5 square"></div>
-            <div class="column-6 square"></div>
-            <div class="column-7 square"></div>
-            <div class="column-8 square"></div>
+          <div className="my-row row-6">
+            <div className="column-0 square"></div>
+            <div className="column-1 square"></div>
+            <div className="column-2 square"></div>
+            <div className="column-3 square"></div>
+            <div className="column-4 square"></div>
+            <div className="column-5 square"></div>
+            <div className="column-6 square"></div>
+            <div className="column-7 square"></div>
+            <div className="column-8 square"></div>
           </div>
         </div>
       </div>
