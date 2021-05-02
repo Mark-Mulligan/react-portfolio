38f2e3ccbce48730a0a30ca6892a22de9db004bb
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-02 08:05:51 -0500

    inital commit

---
+++ src/components/sections/HeroSection.js
@@ -0,0 +1,98 @@
+import React from "react";
+import "./HeroSection.css";
+
+const HeroSection = () => {
+  return (
+    <div class="hero-section">
+      <div class="animated-img-container">
+        <div class="animated-img">
+          <div class="my-row row-0">
+            <div class="column-0 square"></div>
+            <div class="column-1 square"></div>
+            <div class="column-2 square"></div>
+            <div class="column-3 square"></div>
+            <div class="column-4 square"></div>
+            <div class="column-5 square"></div>
+            <div class="column-6 square"></div>
+            <div class="column-7 square"></div>
+            <div class="column-8 square"></div>
+          </div>
+
+          <div class="my-row row-1">
+            <div class="column-0 square"></div>
+            <div class="column-1 square"></div>
+            <div class="column-2 square"></div>
+            <div class="column-3 square"></div>
+            <div class="column-4 square"></div>
+            <div class="column-5 square"></div>
+            <div class="column-6 square"></div>
+            <div class="column-7 square"></div>
+            <div class="column-8 square"></div>
+          </div>
+
+          <div class="my-row row-2">
+            <div class="column-0 square"></div>
+            <div class="column-1 square"></div>
+            <div class="column-2 square"></div>
+            <div class="column-3 square"></div>
+            <div class="column-4 square"></div>
+            <div class="column-5 square"></div>
+            <div class="column-6 square"></div>
+            <div class="column-7 square"></div>
+            <div class="column-8 square"></div>
+          </div>
+
+          <div class="my-row row-3">
+            <div class="column-0 square"></div>
+            <div class="column-1 square"></div>
+            <div class="column-2 square"></div>
+            <div class="column-3 square"></div>
+            <div class="column-4 square"></div>
+            <div class="column-5 square"></div>
+            <div class="column-6 square"></div>
+            <div class="column-7 square"></div>
+            <div class="column-8 square"></div>
+          </div>
+
+          <div class="my-row row-4">
+            <div class="column-0 square"></div>
+            <div class="column-1 square"></div>
+            <div class="column-2 square"></div>
+            <div class="column-3 square"></div>
+            <div class="column-4 square"></div>
+            <div class="column-5 square"></div>
+            <div class="column-6 square"></div>
+            <div class="column-7 square"></div>
+            <div class="column-8 square"></div>
+          </div>
+
+          <div class="my-row row-5">
+            <div class="column-0 square"></div>
+            <div class="column-1 square"></div>
+            <div class="column-2 square"></div>
+            <div class="column-3 square"></div>
+            <div class="column-4 square"></div>
+            <div class="column-5 square"></div>
+            <div class="column-6 square"></div>
+            <div class="column-7 square"></div>
+            <div class="column-8 square"></div>
+          </div>
+
+          <div class="my-row row-6">
+            <div class="column-0 square"></div>
+            <div class="column-1 square"></div>
+            <div class="column-2 square"></div>
+            <div class="column-3 square"></div>
+            <div class="column-4 square"></div>
+            <div class="column-5 square"></div>
+            <div class="column-6 square"></div>
+            <div class="column-7 square"></div>
+            <div class="column-8 square"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
