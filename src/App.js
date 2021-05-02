8e1ee0d53bec7b3738e3bdb794dc663fe96b926e
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-02 18:13:49 -0500

    added quote

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import "./components/sections/HeroSection";
 import HeroSection from './components/sections/HeroSection';
+import AboutSection from "./components/sections/AboutSection";
 
 function App() {
   return (
     <div class="app">
       <HeroSection />
+      <AboutSection />
     </div>
   );
 }
---
+++ src/components/sections/AboutSection.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './AboutSection.css';
+
+const AboutSection = () => {
+  return (
+    <section className="about-section">
+      <div className="quote-container">
+        <h2>Making the <span class="light-blue">simple</span> complicated</h2>
+        <h2>is commonplace</h2>
+        <h2>Make the complicated <span class="light-blue">simple</span>,</h2>
+        <h2>that's <span class="light-blue">creativity</span>.</h2>
+        <h3 class="text-center">- Charles Mingus</h3>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection;
