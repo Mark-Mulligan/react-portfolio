61499657cb7fa8740fe29470d8f306971f26867b
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-02 22:03:00 -0500

    adjusted css

--- src/components/sections/QuoteSection.js
+++ src/components/sections/QuoteSection.js
@@ -6,7 +6,7 @@ const QuoteSection = () => {
     <section className="quote-section">
       <div className="quote-container">
         <h2>Making the <span class="light-blue">simple</span> complicated</h2>
-        <h2>is commonplace</h2>
+        <h2>is commonplace.</h2>
         <h2>Make the complicated <span class="light-blue">simple</span>,</h2>
         <h2>that's <span class="light-blue">creativity</span>.</h2>
         <h3 class="text-center">- Charles Mingus</h3>
