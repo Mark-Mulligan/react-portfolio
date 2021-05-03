8d7ce2ad32c24a7e4c8241c1a7d7e26088d0b9c7
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-02 19:33:35 -0500

    about section

--- src/components/sections/AboutSection.js
+++ src/components/sections/AboutSection.js
@@ -6,6 +6,28 @@ const AboutSection = () => {
       <div className="container">
         <h2 className="text-center">About</h2>
       </div>
+      <div className="container">
+      <div className="row text-center">
+        <div className="col">
+          <h4>Simple</h4>
+          <p>Less is more. Only add elements that serve a purpose.</p>
+        </div>
+        <div className="col">
+          <h4>Dynamic</h4>
+          <p>Motion creates engagement which is the key to an effective website.</p>
+        </div>
+        <div className="col">
+          <h4>Responsive</h4>
+          <p>My designs work on all screens sizes, from mobile to desktop.</p>
+        </div>
+        <div className="col">
+          
+          <h4>Stylelish</h4>
+          <p>First impressions, especially on the web, are everything.  </p>
+        </div>
+      </div>
+        
+      </div>
     </section>
   )
 }
