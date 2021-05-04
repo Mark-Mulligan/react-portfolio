032d8d067bae9717e74b857f81f9e4d3f27554de
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-04 09:01:57 -0500

    added description

--- src/components/sections/ProjectsSection.js
+++ src/components/sections/ProjectsSection.js
@@ -10,7 +10,22 @@ const ProjectsSection = () => {
       <div className="container projects-container p-0">
         <div className="projects-row">
           <div className="left-picture"></div>
-          <div className="right-description"></div>
+          <div className="right-description">
+            <div>
+            <h3>Sneakerheads</h3>
+            <p>Social Media sight for those that love shoes.</p>
+            <div class="project-links-container">
+              <div>
+                <a href="https://sneaker-heads.herokuapp.com" target="_blank">View Project</a>
+              </div>
+              <div>
+                <a href="https://github.com/hugo2211/Sneaker_Head" target="_blank">Github</a>
+              </div>
+            </div>
+            </div>
+            
+
+          </div>
         </div>
 
         <div className="projects-row">
