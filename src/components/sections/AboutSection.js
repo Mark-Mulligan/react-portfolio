7229e78c50ad9db4ebea93b0524a34a1c5c48594
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-03 23:04:53 -0500

    added section

--- src/components/sections/AboutSection.js
+++ src/components/sections/AboutSection.js
@@ -9,11 +9,28 @@ const AboutSection = () => {
   return (
     <section className="about-section">
       <div className="container">
-        <h2 className="text-center">About</h2>
+        <h2 className="text-center">My Approach</h2>
         <hr />
       </div>
 
       <PictureGrid />
+
+      <div className="container">
+        <div class="row">
+          <div class="col">
+            <h3 class="text-center">About</h3>
+            <p class="bio">
+              Hey, I'm Mark, a Front End Developer based in Dallas TX. I love to
+              build amazing websites with simple, intuitive user interfaces. I
+              am always looking for new things to learn and projects to take on.
+              Let's work together.
+            </p>
+          </div>
+          <div class="col">
+            <h3 class="text-center">Skills</h3>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
