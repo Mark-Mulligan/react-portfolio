3d2d810049615c9c5995e2fa97c7521330af974b
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-04 08:19:15 -0500

    added projects section breakpoitins

--- src/components/sections/ProjectsSection.js
+++ src/components/sections/ProjectsSection.js
@@ -7,24 +7,31 @@ const ProjectsSection = () => {
         <h2 className="text-center">Projects</h2>
       </div>
 
-      <div class="fixed-background project-1-image">
-        <div class="project-description">
-          <div class="fade-in-out-effect description-text-container">
-            <h2>SneakerHeads</h2>
-            <p>Social Media sight for those that love shoes.</p>
-            <div class="project-links-container">
-              <div>
-                <a href="https://sneaker-heads.herokuapp.com" target="_blank">View Project</a>
-              </div>
-              <div>
-                <a href="https://github.com/hugo2211/Sneaker_Head" target="_blank">Github</a>
-              </div>
-            </div>
-          </div>
+      <div className="container projects-container p-0">
+        <div className="projects-row">
+          <div className="left-picture"></div>
+          <div className="right-description"></div>
         </div>
+
+        <div className="projects-row">
+          <div className="left-picture-2"></div>
+          <div className="right-description-2"></div>
+        </div>
+
+        <div className="projects-row">
+          <div className="left-picture project-2"></div>
+          <div className="right-description"></div>
+        </div>
+
+        <div className="projects-row">
+          <div className="left-picture-2"></div>
+          <div className="right-description-2"></div>
+        </div>
+
+        
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default ProjectsSection;
